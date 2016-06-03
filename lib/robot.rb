e9f584f55d27bc0880fa1718444764fc1b088d60
monicamow <mow.monica@gmail.com>
2016-06-02 20:55:51 -0700

    WHERE ARE MY FILES

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,15 +1,14 @@
 class Robot
 
-  attr_reader :position, :items, :items_weight, :health, :hitpoints, :equipped_weapon, :shield
+  attr_reader :position, :items, :health, :hitpoints, :equipped_weapon
   attr_writer :equipped_weapon
 
   def initialize
     @position = [0,0]
     @items = []
-    @items_weight = 0
+    #@items_weight = 0
     @health = 100
     @hitpoints = 5
-    @shield = 50
   end
 
   def move_left
@@ -30,6 +29,9 @@ class Robot
 
   # fix 03
   def pick_up(item)
+    if item.is_a?(BoxOfBolts) && health <= 80
+      item.feed(self)
+    end
     if item.is_a? Weapon
       @equipped_weapon = item
     end
@@ -68,12 +70,30 @@ class Robot
     heal(amount)
   end
 
+  def can_attack?(other_robot)
+    (self.position[0] == other_robot.position[0] + 1) || 
+    (self.position[1] == other_robot.position[1] + 1) || 
+    (self.position[0] == other_robot.position[0] - 1) || 
+    (self.position[1] == other_robot.position[1] - 1) || 
+    (self.position == other_robot.position) || 
+    @equipped_weapon.is_a?(Grenade) && (
+    (self.position[0] == other_robot.position[0] + 2) || 
+    (self.position[1] == other_robot.position[1] + 2) || 
+    (self.position[0] == other_robot.position[0] - 2) || 
+    (self.position[1] == other_robot.position[1] - 2)
+    )
+
+  end
+
   def attack(other_robot)
-    unless @equipped_weapon.nil?
-      @equipped_weapon.hit(other_robot)
-    else
-      other_robot.wound(hitpoints)
+    if can_attack?(other_robot)
+      unless @equipped_weapon.nil?
+        @equipped_weapon.hit(other_robot)
+      else
+        other_robot.wound(hitpoints)
+      end
     end
+    @equipped_weapon = nil if @equipped_weapon.is_a? Grenade
   end
 
   # ENHANCEMENT 2
